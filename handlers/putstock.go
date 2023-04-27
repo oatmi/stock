8aed51ee0b81c1bbaf5b4c37da79e56dbdcbe8db
Troy <gnayoat@gmail.com>
2023-04-27 08:16:27 +0800

    put stock

--- handlers/putstock.go
+++ handlers/putstock.go
@@ -1,25 +1,19 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/oatmi/stock/data"
 	"github.com/oatmi/stock/data/sqlite"
-	"github.com/oatmi/stock/lib"
 	"github.com/spf13/cast"
 )
 
 const wechatBotWebHook = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3abf3cf5-ba21-44e8-b5d8-60abd83f9c7a"
 
 type PutStockRequest struct {
-	Stock     []Stock `json:"stock"`
-	BatchNoIn string  `json:"batch_no_in"`
-}
-
-type Stock struct {
+	BatchNoIn        string `json:"batch_no_in"`
 	Name             string `json:"name"`
 	ProductType      string `json:"product_type"`
 	ProductAttr      string `json:"product_attr"`
@@ -37,8 +31,8 @@ type Stock struct {
 }
 
 func PutStock(c *gin.Context) {
-	var stocks PutStockRequest
-	err := c.BindJSON(&stocks)
+	var stock PutStockRequest
+	err := c.BindJSON(&stock)
 	if err != nil {
 		c.JSON(http.StatusOK, AisudaiResponse{Status: 1, Message: "[E100] 参数错误"})
 		return
@@ -46,53 +40,45 @@ func PutStock(c *gin.Context) {
 
 	query := sqlite.New(data.Sqlite3)
 
-	application := sqlite.CreateStockApplicationParams{
-		ApplicationDate: lib.CurrentDate(),
-		BatchNoIn:       stocks.BatchNoIn,
-		Status:          1,       // 1: initiate, 2: wait approve, 3: prooved, 4: rejected
-		ApplicationUser: "admin", // TODO
-		CreateDate:      lib.CurrentDate(),
+	// application := sqlite.CreateStockApplicationParams{
+	// 	ApplicationDate: lib.CurrentDate(),
+	// 	BatchNoIn:       stocks.BatchNoIn,
+	// 	Status:          1,       // 1: initiate, 2: wait approve, 3: prooved, 4: rejected
+	// 	ApplicationUser: "admin", // TODO
+	// 	CreateDate:      lib.CurrentDate(),
+	// }
+	// err = query.CreateStockApplication(c, application)
+	// if err != nil {
+	// 	c.JSON(http.StatusOK, AisudaiResponse{Status: 1, Message: "[E101] 提交申请失败" + err.Error()})
+	// 	return
+	// }
+
+	createParam := sqlite.CreateStockParams{
+		Status:           2, // 1: ok, 2: waitIN, 3: outed 4: declined
+		Name:             stock.Name,
+		ProductType:      cast.ToInt32(stock.ProductType),
+		ProductAttr:      cast.ToInt32(stock.ProductAttr),
+		Supplier:         stock.Supplier,
+		Model:            stock.Model,
+		Unit:             stock.Unit,
+		Price:            cast.ToInt32(stock.Price),
+		BatchNoIn:        stock.BatchNoIn,
+		WayIn:            cast.ToInt32(stock.WayIn),
+		Location:         cast.ToInt32(stock.Location),
+		BatchNoProduce:   stock.BatchNoProduce,
+		ProduceDate:      cast.ToInt32(stock.ProduceDate),
+		DisinfectionNo:   stock.DisinfectionNo,
+		DisinfectionDate: cast.ToInt32(stock.DisinfectionDate),
+		StockDate:        int32(time.Now().Unix()),
+		StockNum:         cast.ToInt32(stock.StockNum),
+		CurrentNum:       cast.ToInt32(stock.StockNum),
+		Value:            cast.ToInt32(stock.StockNum) * cast.ToInt32(stock.Price),
 	}
-	err = query.CreateStockApplication(c, application)
-	if err != nil {
+	err = query.CreateStock(c, createParam)
+	if err == nil {
 		c.JSON(http.StatusOK, AisudaiResponse{Status: 1, Message: "[E101] 提交申请失败" + err.Error()})
 		return
 	}
-
-	var countSucc, countFail int
-
-	for _, s := range stocks.Stock {
-		createParam := sqlite.CreateStockParams{
-			Status:           2, // 1: ok, 2: waitIN, 3: outed
-			Name:             s.Name,
-			ProductType:      cast.ToInt32(s.ProductType),
-			ProductAttr:      cast.ToInt32(s.ProductAttr),
-			Supplier:         s.Supplier,
-			Model:            s.Model,
-			Unit:             s.Unit,
-			Price:            cast.ToInt32(s.Price),
-			BatchNoIn:        stocks.BatchNoIn,
-			WayIn:            cast.ToInt32(s.WayIn),
-			Location:         cast.ToInt32(s.Location),
-			BatchNoProduce:   s.BatchNoProduce,
-			ProduceDate:      cast.ToInt32(s.ProduceDate),
-			DisinfectionNo:   s.DisinfectionNo,
-			DisinfectionDate: cast.ToInt32(s.DisinfectionDate),
-			StockDate:        int32(time.Now().Unix()),
-			StockNum:         cast.ToInt32(s.StockNum),
-			CurrentNum:       cast.ToInt32(s.StockNum),
-			Value:            cast.ToInt32(s.StockNum) * cast.ToInt32(s.Price),
-		}
-		err := query.CreateStock(c, createParam)
-		if err == nil {
-			countSucc += 1
-		} else {
-			fmt.Printf("debug: %+v\n", err)
-			countFail += 1
-		}
-	}
-
-	message := fmt.Sprintf("成功提交%d条申请，失败%d条", countSucc, countFail)
 
 	// markdownMessage := wechat.Markdown{
 	// 	Content: wechat.MarkdownTemplate01,
@@ -113,5 +99,5 @@ func PutStock(c *gin.Context) {
 	// 	return
 	// }
 
-	c.JSON(http.StatusOK, AisudaiResponse{Status: 0, Message: message})
+	c.JSON(http.StatusOK, AisudaiResponse{Status: 0, Message: "提交成功"})
 }
